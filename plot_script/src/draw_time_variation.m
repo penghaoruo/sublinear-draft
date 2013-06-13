8bc33c16cfb60a02864c1856874a115a942a6f25
penghaoruo <penghaoruo@gmail.com>
2013-06-14 02:39:17 +0800

    IEEE version

--- plot_script/src/draw_time_variation.m
+++ plot_script/src/draw_time_variation.m
@@ -1,19 +1,26 @@
 clc, clear
-%dataset = '2d';
+dataset = '2d';
 %dataset = '20NewsGroup';
 %dataset = 'Gisette';
 %dataset = 'ECUESpam';
-dataset = 'URL-Reputation';
+%dataset = 'URL-Reputation';
 mat = [dataset,'_time.mat'];
 load(mat);
 figure; 
-set(gcf,'Position',[10,10,1800,900], 'color','w');
+set(gcf,'Position',[100,100,1000,750], 'color','w');
+LineWidth=3;
+MarkerSize=8;
 x=y(4:6,:);
-x=x*4;
+t=1:6;
+%x=x*4;
 b=bar(x);
-grid on;
-set(gca,'XTickLabel',{'PSUBPLR-MR','PGDPLR-SPARK','PSUBPLR-SPARK'})
+plot(t,x(1,:),'-ok','LineWidth',LineWidth,'MarkerSize',MarkerSize);
+hold on;
+plot(t,x(2,:),'--xb','LineWidth',LineWidth,'MarkerSize',MarkerSize);
+hold on;
+plot(t,x(3,:),'-vr','LineWidth',LineWidth,'MarkerSize',MarkerSize);
+hold on;
 xlabel('Number of Nodes Used','Fontsize',25,'fontweight','b');
 ylabel('Running Time per Iteration/s','Fontsize',25,'fontweight','b');
-set(gca,'fontsize',18,'fontweight','b');
-legend('1 node','2 nodes','3 nodes','4 nodes','5 nodes','6 nodes');
+set(gca,'fontsize',25,'fontweight','b');
+legend('PSUBPLR-MR','PGDPLR-SPARK','PSUBPLR-SPARK');
